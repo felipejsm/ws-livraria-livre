58ed97273bac5bad4196acd05567c41037eab4e4
felipejsm <felipejsm41@gmail.com>
2017-04-04 21:21:20 -0300

    Inicio do projeto

---
+++ src/main/java/com/livre/WsLivrariaLivreApplication.java
@@ -0,0 +1,12 @@
+package com.livre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsLivrariaLivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsLivrariaLivreApplication.class, args);
+	}
+}
