80e4a5e3dfd94e030fcada4318b3eafa234344f6
felipejsm <felipejsm41@gmail.com>
2017-04-23 12:30:34 -0300

    targenamespace alterado

--- src/main/java/br/com/livre/livraria/Livro.java
+++ src/main/java/br/com/livre/livraria/Livro.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2017.04.10 às 08:42:53 PM BRT 
+// Gerado em: 2017.04.23 às 11:55:28 AM BRT 
 //
 
 
