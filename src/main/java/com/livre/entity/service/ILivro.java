c4768cf10437d04ac10a9aac5b0348f30ca14366
felipejsm <felipejsm41@gmail.com>
2017-04-24 21:01:17 -0300

    Alteracao parametro para String

--- src/main/java/com/livre/entity/service/ILivro.java
+++ src/main/java/com/livre/entity/service/ILivro.java
@@ -4,8 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.livre.entity.domain.LivroDomain;
 
-import br.com.livre.livraria.GetDadosLivroRequest;
-
 public interface ILivro extends CrudRepository<LivroDomain, Long>{
-	public LivroDomain findByNome(GetDadosLivroRequest request);
+	public LivroDomain findByNome(String request);
 }
